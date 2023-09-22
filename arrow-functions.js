0385c24a41da1d0d82b52ad5e6939d0f3737b676
josipcodes <j.potnar@gmail.com>
2023-09-22 13:05:39 +0100

    Write arrow function examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,41 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers1 = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum1 = addTwoNumbers1(3, 5);
+console.log(sum1);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum2);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("Hello there!");
+
+const sayHello = () => console.log("hello")
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multi-line string!
+    </p>`
+)
+console.log(returnMultipleLines())
---
+++ node_c578bbac7c982.tmp.js
@@ -0,0 +1,21 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node arrow-functions.js in the terminal
+ */
+
+// Vanilla JavaScript Function
+
+function addTwoNumbers(a, b) {
+    // Code block
+    return a + b
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum)
+
+// Arrow Function With Parameters
+
+// Single Line Arrow Function With Parameters
+
+// Implicit Returns
+
+// Returning Multiple Lines
